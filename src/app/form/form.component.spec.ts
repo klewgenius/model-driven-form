d231297d9155fbdb34b3149f65cbd97afd5778ad
Andrés Baez <abaez@carrasco.uruit.com>
2017-09-17 21:11:07 -0300

    Init

---
+++ src/app/form/form.component.spec.ts
@@ -0,0 +1,127 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './form.component';
+
+describe('FormComponent', () => {
+  let component: FormComponent;
+  let fixture: ComponentFixture<FormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FormComponent ],
+      imports: [ReactiveFormsModule]      
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('initial name should be empty', () => {
+
+    expect(component.form.get('name').value).toEqual('');
+  });
+
+  
+
+  describe('when Administrator is checked', () => {
+    
+    beforeEach(()=>{
+      component.form
+      .get('roles')
+      .get('admin')
+      .setValue(true); 
+    });
+
+    it('ExecutionManager is checked too', () => {
+      //Assert
+      expect(component.form.get('roles').get('executionmanager').value).toBeTruthy();      
+    });
+
+    it('ManagementStudio is checked too', () => {
+      //Assert
+      expect(component.form.get('roles').get('managementstudio').value).toBeTruthy();
+    });
+
+  });
+
+  describe('when Analyze is checked', () => {
+
+    beforeEach(()=>
+    {
+      //Admin, EM and MS are selected
+      component.form
+      .get('roles')
+      .patchValue({
+        'admin':true
+      });
+
+      //When I click Analyze it should clear the others.
+      component.form
+      .get('roles')
+      .get('analyze')
+      .setValue(true); 
+    });
+
+    it('Administrator is unchecked', () => {
+      //Assert
+      expect(component.form.get('roles').get('admin').value).toBeFalsy();      
+    });
+  });
+
+  describe('when ExecutionManager is unchecked', () => {
+    
+        beforeEach(()=>
+        {
+          //Admin, EM and MS are selected
+          component.form
+          .get('roles')
+          .patchValue({
+            'admin':true
+          });
+    
+          //When I click Analyze it should clear the others.
+          component.form
+          .get('roles')
+          .get('executionmanager')
+          .setValue(false); 
+        });
+    
+        it('Administrator is unchecked too', () => {
+          //Assert
+          expect(component.form.get('roles').get('admin').value).toBeFalsy();      
+        });
+      });
+
+      describe('when ManagementStudio is unchecked', () => {
+        
+            beforeEach(()=>
+            {
+              //Admin, EM and MS are selected
+              component.form
+              .get('roles')
+              .patchValue({
+                'admin':true
+              });
+        
+              //When I click Analyze it should clear the others.
+              component.form
+              .get('roles')
+              .get('managementstudio')
+              .setValue(false); 
+            });
+        
+            it('Administrator is unchecked too', () => {
+              //Assert
+              expect(component.form.get('roles').get('admin').value).toBeFalsy();      
+            });
+          });
+  
+});
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,91 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+
+export class FormComponent implements OnInit {
+
+  form: FormGroup;
+
+  ngOnInit(){
+    this.onChanges();
+  }
+
+  constructor(@Inject(FormBuilder) private fb: FormBuilder) { 
+    
+    this.form = fb.group({
+      name: '',
+      roles: this.createRolesGroup()
+    });
+    
+  }
+
+  private createRolesGroup() {
+    const group = this.fb.group({});
+    this.rules.forEach(control => group.addControl(control.role, this.fb.control(control.default)));
+    return group;
+  }
+  
+    
+  onChanges()
+  {
+    
+    this.rules.forEach((rule) => 
+    {
+      this.form.get(['roles', rule.role])
+      .valueChanges
+      .subscribe(val => { 
+          this.form.get('roles').patchValue(val ? rule.checked: rule.unchecked);
+      });
+    });
+
+  }
+
+
+  readonly rules = [
+    {
+      role: 'admin',
+      default: false,
+      checked: 
+      {        
+        "executionmanager": true,
+        "managementstudio": true,
+        "analyze": false
+      },
+      unchecked: {}
+    },
+    {
+      role: 'analyze',
+      default: false,
+      checked:      
+      {
+        'admin': false,
+        'executionmanager': false,
+        'managementstudio': false
+      },
+      unchecked: {}
+    },
+    {
+      role: 'executionmanager',
+      default: false,
+      checked: {},      
+      unchecked:
+      {
+        'admin': false
+      }
+    },
+    {
+      role: 'managementstudio',
+      default: false,
+      checked: {},      
+      unchecked:
+      {
+        'admin': false
+      }
+    }
+  ];
+}
